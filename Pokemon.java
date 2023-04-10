13e193f74244ca98aa994b75aed6c62374d72818
Alfrickr <abaime@mail.umw.edu>
2023-04-10 16:42:05 -0400

    add name

--- Pokemon.java
+++ Pokemon.java
@@ -1,5 +1,4 @@
 import java.io.ObjectInputFilter;
-import java.util.ArrayList;
 
 public class Pokemon {
     private String name;
@@ -19,6 +18,37 @@ public class Pokemon {
 
     // Doesn't exist..
     private Ability ability;
-    //private ArrayList<Move> = new ArrayList<Move>();
+    private ArrayList<Move> = new ArrayList<>();
     private Item heldItem;
+
+    private double againstBug;
+    private double againstDark;
+    private double againstDragon;
+    private double againstElectric;
+    private double againstFairy;
+    private double againstFight;
+    private double againstFire;
+    private double againstFlying;
+    private double againstGhost;
+    private double againstGrass;
+    private double againstGround;
+    private double againstIce;
+    private double againstNormal;
+    private double againstPoison;
+    private double againstPsychic;
+    private double againstRock;
+    private double againstSteel;
+    private double againstWater;
+
+
+
+
+
+
+
+    public Pokemon(int number)//makes pokemon from number
+    {
+
+    }
+
 }
