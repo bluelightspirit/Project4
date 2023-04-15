c617ede2e24330867dc60c6cd3be50b322b3e939
bluelightspirit <bluelittlebears@hotmail.com>
2023-04-15 19:13:20 -0400

    created base GUI for pokemane battle simulator

--- PokeFrame.java
+++ PokeFrame.java
@@ -3,9 +3,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.BufferedImageOp;
+import java.awt.image.TileObserver;
 import java.io.IOException;
 import java.net.URL;
+import javax.swing.border.TitledBorder;
 import java.nio.Buffer;
+import java.awt.LayoutManager;
+import java.io.Serializable;
+
 
 public class PokeFrame extends JFrame {
     PokeFrame() {
@@ -13,25 +18,24 @@ public class PokeFrame extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
 
-        this.setSize(500,500);
 
         // full screen
 //        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
 //        this.setUndecorated(true);
 
-        this.setVisible(true);
-        BufferedImage urlImage = getImage("https://img.pokemondb.net/sprites/sword-shield/icon/charizard.png");
+//        this.setVisible(true);
+//        BufferedImage urlImage = getImage("https://cdn.discordapp.com/avatars/262317813396537345/7f5d50988b402fbd1c853e37ab3c27a1?size=1024");
+//
+//        ImageIcon image = new ImageIcon(urlImage);
+//        this.setIconImage(image.getImage());
+//        this.getContentPane().setBackground(new Color(255, 255, 255));
+//
+//        JLabel label = new JLabel();
+//        label.setText("test");
+//        ImageIcon icon2 = new ImageIcon(getImage("https://img.pokemondb.net/sprites/sword-shield/icon/venusaur.png"));
+//        label.setIcon(icon2);
 
-        ImageIcon image = new ImageIcon(urlImage);
-        this.setIconImage(image.getImage());
-        this.getContentPane().setBackground(new Color(255, 255, 255));
-
-        JLabel label = new JLabel();
-        label.setText("test");
-        ImageIcon icon = new ImageIcon(getImage("https://img.pokemondb.net/sprites/sword-shield/icon/venusaur.png"));
-        label.setIcon(icon);
-
-        this.add(label);
+        // this.add(label);
     }
 
     // get image from link given
@@ -45,15 +49,54 @@ public class PokeFrame extends JFrame {
         }
     }
 
+    // get image from pokemon name given
+    public static BufferedImage getSprite(String pokemonName) {
+        BufferedImage urlImage;
+        String link = "https://img.pokemondb.net/sprites/sword-shield/icon/".concat(pokemonName + ".png");
+        return getImage(link);
+    }
+
     public static void main(String[] args) {
         JFrame frame = new PokeFrame();
 
-        JLabel label = new JLabel();
-        label.setText("test");
-        ImageIcon icon = new ImageIcon(getImage("https://img.pokemondb.net/sprites/sword-shield/icon/venusaur.png"));
-        label.setIcon(icon);
+        JPanel playerPokemonPanel = new JPanel();
+        JLabel playerLabel = new JLabel();
+        int tempHp = 0;
+        playerLabel.setText("<html>Charizard <br />HP: " + tempHp + "</html>");
+        playerLabel.setForeground(Color.white);
+        ImageIcon icon = new ImageIcon(getSprite("charizard"));
+        playerLabel.setIcon(icon);
+        playerPokemonPanel.add(playerLabel);
+        playerPokemonPanel.setBackground(Color.blue);
 
+
+        JPanel enemyPokemonPanel = new JPanel();
+        JLabel enemyLabel = new JLabel();
+        enemyLabel.setText("<html>Pikachu<br />HP: " + tempHp + "</html>");
+        enemyLabel.setForeground(Color.white);
+        icon = new ImageIcon(getSprite("pikachu"));
+        enemyLabel.setIcon(icon);
+        enemyPokemonPanel.add(enemyLabel);
+        enemyPokemonPanel.setBackground(Color.red);
+
+
+        JButton move1 = new JButton("Move 1");
+        JButton move2 = new JButton("Move 2");
+        JButton move3 = new JButton("Move 3");
+        JButton move4 = new JButton("Move 4");
+        JButton swap = new JButton("Swap");
+
+        JPanel movesPanel = new JPanel();
+        movesPanel.add(move1); movesPanel.add(move2); movesPanel.add(move3); movesPanel.add(move4); movesPanel.add(swap);
+        movesPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        movesPanel.setBackground(Color.cyan);
+
+        frame.setLayout(new GridLayout(3, 1));
+        frame.add(enemyPokemonPanel);
+        frame.add(playerPokemonPanel);
+        frame.add(movesPanel);
+
+        frame.pack();
         frame.setVisible(true);
-        frame.add(label);
     }
 }
