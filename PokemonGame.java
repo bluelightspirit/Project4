2b497355fb63961e571f546c2f21434a123823e2
Alfrickr <abaime@mail.umw.edu>
2023-04-15 17:41:31 -0400

    notes for how to create main

--- PokemonGame.java
+++ PokemonGame.java
@@ -1,5 +1,32 @@
-public class PokemonGame {
-    public static void main(String[] args) {
+import java.io.ObjectInputFilter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+public class PokemonGame
+{
+    public static void main(String[] args)
+    {
+        //have it create file
+        //pokemon must be selected by their pokedex number
+        //user will then be prompted to enter moves for pokemon
+        //moves will be accepted if they are a type that the pokemon is
+        //in file it will have the 6 player pokemon with their moves
+        /* example file entry for sylveon and vaporeon
+            700
+            Charm
+            Baby-Doll Eyes
+            Draining Kiss
+            Moonblast
+
+            134
+            Tackle
+            Water Pulse
+            Hydro Pump
+            Baby-Doll Eyes
+        */
+
+
+        // file will also have 6 randomly generated pokemon with random moves for the enemy
 
     }
 }
