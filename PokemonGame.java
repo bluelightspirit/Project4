a99572c5375ba69af3580a6c3b06c46f01da1bd7
Alfrickr <abaime@mail.umw.edu>
2023-04-16 13:39:03 -0400

    added more to main method

--- PokemonGame.java
+++ PokemonGame.java
@@ -13,10 +13,11 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.File;
+import java.util.random.*;
+import java.lang.Math;
 public class PokemonGame
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws FileNotFoundException {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter name of game file");
         System.out.println("");
@@ -26,47 +27,89 @@ public class PokemonGame
         input = input.toLowerCase();
         String fileName = "./PokemonData/"+ input + ".txt";
         System.out.println("Game file " + fileName + "was made");
+        PrintWriter print = new PrintWriter(fileName);
         int count = 0;
         //get list of pokemon and list of moves
         ArrayList<String> names = new ArrayList<String>();
         ArrayList<String> moveNames = new ArrayList<String>();
-        try
-        {
+        try {
             Scanner fIn = new Scanner(new FileInputStream("name.txt"));
-            while(fIn.hasNext())
-            {
+            while(fIn.hasNext()) {
                 names.add(fIn.nextLine());
             }
         }
-        catch(Exception e)
-        {
+        catch(Exception e) {
             System.out.println("error getting data");
         }
-        try
-        {
+        try {
             Scanner fIn = new Scanner(new FileInputStream("moveNames.txt"));
-            while(fIn.hasNext())
-            {
+            while(fIn.hasNext()) {
                 moveNames.add(fIn.nextLine());
             }
         }
-        catch(Exception e)
-        {
+        catch(Exception e) {
             System.out.println("error getting data");
         }
-
-
-
-        while(count < 6)
-        {
-            if(count < 3) //player pokemon
-            {
-
+        //Scanner in = new Scanner(System.in);
+        while(count < 6) {
+            if(count < 3) {
+                System.out.println("Please enter name of pokemon to be added to your team. Or type R to let RNG decide pokemon");
             }
-            else //enemy pokemon
-            {
-
+            else{
+            System.out.println("Please enter name of pokemon to be added to enemy team. Or type R to let RNG decide pokemon");
             }
+            String response = in.next();
+
+            if(response.equals("R")) {//rng decide
+                 int i = (int)(Math.random() * 801);
+                 int mov1 =  (int)(Math.random() * 502);
+                 int mov2 =  (int)(Math.random() * 502);
+                 int mov3 =  (int)(Math.random() * 502);
+                 int mov4 =  (int)(Math.random() * 502);
+                 print.println(names.get(i));
+                 print.println(names.get(mov1));
+                 print.println(names.get(mov2));
+                 print.println(names.get(mov3));
+                 print.println(names.get(mov4));
+                 count++;
+                }
+                else {
+                    while (!names.contains(response)) {
+                        if(count < 3) {
+                            System.out.println("Please enter name of pokemon to be added to your team");
+                        }
+                        else{
+                            System.out.println("Please enter name of pokemon to be added to enemy team");
+                        }
+                        response = in.next();
+                    }
+                    print.println(response);
+                    // add response
+                    System.out.println("Enter name of move to be added or type R to let RNG decide");
+                    response = in.next();
+                    int count2 = 0;
+                    if(response.equals("R")) {//rng decide
+                        int mov1 = (int) (Math.random() * 502);
+                        int mov2 = (int) (Math.random() * 502);
+                        int mov3 = (int) (Math.random() * 502);
+                        int mov4 = (int) (Math.random() * 502);
+                        print.println(names.get(mov1));
+                        print.println(names.get(mov2));
+                        print.println(names.get(mov3));
+                        print.println(names.get(mov4));
+                        count++;
+                    }
+                    while (!moveNames.contains(response) && count2 < 4) {
+                        System.out.println("Enter name of move to be added");
+                        response = in.next();
+                        if(moveNames.contains(response))
+                        {
+                            print.println(response);
+                            count2++;
+                        }
+                    }
+                    count++;
+                }
         }
 
 
