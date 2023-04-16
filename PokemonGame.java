c2e48d4d0b94ad85d08ebdf887981e24fe982939
Alfrickr <abaime@mail.umw.edu>
2023-04-16 13:56:11 -0400

    fixed issue printwriter closing

--- PokemonGame.java
+++ PokemonGame.java
@@ -122,36 +122,6 @@ public class PokemonGame
             }
 
         }
-
-
-
-
-
-
-
-        //have it create file
-        //pokemon must be selected by their pokedex number
-        //user will then be prompted to enter moves for pokemon
-        //moves will be accepted if they are a type that the pokemon is
-        //in file it will have the 6 player pokemon with their moves
-        /* example file entry for sylveon and vaporeon
-            700
-            50
-            Charm
-            Baby-Doll Eyes
-            Draining Kiss
-            Moonblast
-
-            134
-            50
-           Tackle
-            Water Pulse
-            Hydro Pump
-            Baby-Doll Eyes
-        */
-
-
-        // file will also have 6 randomly generated pokemon with random moves for the enemy
-
+        print.close();
     }
 }
