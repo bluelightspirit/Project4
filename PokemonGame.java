ccf13d2e31cd5d6d2972123d8039f42e0716c8a4
bluelightspirit <bluelittlebears@hotmail.com>
2023-04-16 23:25:15 -0400

    made PokeGUI accessible within PokemonGame.java

--- PokemonGame.java
+++ PokemonGame.java
@@ -167,6 +167,7 @@ public class PokemonGame
         System.out.println(enemyPokemonArrayList.get(2).getName());
 
         JFrame pokeGUI = new PokeGUI();
-
+        ((PokeGUI) pokeGUI).setPlayerPokemonName(playerPokemonArrayList.get(0).getName());
+        ((PokeGUI) pokeGUI).updatePlayerLabel("WAITING");
     }
 }
