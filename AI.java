306e92da05904d98f274c86d6feb22218b35445c
Joey <joseph.a.ryerson@gmail.com>
2023-04-17 21:23:04 -0400

    Added the random move for AI

--- AI.java
+++ AI.java
@@ -3,19 +3,23 @@ public class AI {
 
     public static Move decideMove(Pokemon pokemon) {
         int choice = (int)(Math.random()*(4)+1);
-        System.out.println(choice);
         switch (choice) {
-            case 1:
+            case 1 -> {
                 return pokemon.getMove1();
-            case 2:
+            }
+            case 2 -> {
                 return pokemon.getMove2();
-            case 3:
+            }
+            case 3 -> {
                 return pokemon.getMove3();
-            case 4:
+            }
+            case 4 -> {
                 return pokemon.getMove4();
-            default:
+            }
+            default -> {
                 System.out.println("AI class | decideMoveDefault error");
                 return pokemon.getMove1();
+            }
         }
     }
 }
